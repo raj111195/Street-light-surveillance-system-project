2382f2a7922e5fe80fa9512ef81b838a2b7c6cf7
Rajkumar S <rajkumar.df54@gmail.com>
2020-09-06 01:13:47 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+
+define ('hostname','localhost');
+define ('user','root');
+define ('password','');
+define ('databaseName','emerge');	
+$connect = mysqli_connect(hostname,user,password,databaseName);
+	     	
+?>
